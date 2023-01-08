4ffb6159c6fd9a6c598899ce6fbdca55cc0b5201
Jorge <jorge6alberto@gmail.com>
2023-01-08 11:45:15 -0600

    jorge/deployment-refactors-1 Add relative paths of assets

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,28 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js',
+                
+                'resources/js/admin_view_config.js',
+                'resources/js/admin_view_groups.js',
+                'resources/js/admin_view_solicitudes.js',
+                'resources/js/environment.js',
+                'resources/js/fetch_js_api.js',
+                'resources/js/home_view.js',
+                'resources/js/login_view.js',
+                'resources/js/paginate.js',
+                'resources/js/signup_view.js',
+                'resources/js/solicitude_admin_view.js',
+                'resources/js/solicitude_student_view.js',
+
+                'resources/css/admin_home_view.css',
+                'resources/css/application-section.css',
+                'resources/css/solicitude-view.css',
+                'resources/css/student-home-view.css',
+                'resources/css/topbar-environment.css',
+            ],
             refresh: true,
         }),
     ],
